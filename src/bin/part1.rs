65473611ae1e1a12253983fc283f462dff3ea852
Amitav Chris Mostafa <a.mostafa@cheil.com>
2023-12-22 09:57:49 +0100

    Setup rust env

---
+++ src/bin/part1.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Starting Part 1");
+}
+fn part1(input: &str) -> String {
+    todo!()
+}
+// Testing in rust
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 69 + 42;
+        assert_eq!(result, 111)
+    }
+}
